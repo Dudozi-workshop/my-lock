df773a7d0d1248f6404f0c678a4122ee8483a7cf
Dudozi-workshop <rlatkd5959@gmail.com>
2026-09-20 02:01:08 +0900

    feat: add dedicated lock Dart entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/widgets.dart';
 
 import 'app/my_lock_app.dart';
+import 'app/my_lock_lock_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyLockApp());
+}
+
+
+@pragma('vm:entry-point')
+void lockMain() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyLockLockApp());
 }
