a1dbe3c8d8c7f2c828e0cd0b9e8c4eeecf64e876
Dudozi-workshop <rlatkd5959@gmail.com>
2026-09-20 02:08:27 +0900

    feat: sync relock policy to Android monitor

--- lib/lock_engine/platform_lock_bridge.dart
+++ lib/lock_engine/platform_lock_bridge.dart
@@ -51,6 +51,8 @@ abstract class PlatformLockBridge {
 
   Future<void> syncExperimentalScreenLock(bool enabled);
 
+  Future<void> syncRelockPolicy(String policy);
+
   Future<void> notifyUnlockGranted(String appId);
 
   Future<void> presentLockScreen(String appId);
@@ -194,6 +196,15 @@ class MethodChannelPlatformLockBridge implements PlatformLockBridge {
     await _invokeSafely(
       'syncExperimentalScreenLock',
       <String, Object?>{'enabled': enabled},
+    );
+  }
+
+  @override
+  Future<void> syncRelockPolicy(String policy) async {
+    if (kIsWeb) return;
+    await _invokeSafely(
+      'syncRelockPolicy',
+      <String, Object?>{'policy': policy},
     );
   }
 
