234cf94d9f4debc82b1d0dc83eaf4700447e76cb
Dudozi-workshop <rlatkd5959@gmail.com>
2026-09-25 16:31:17 +0900

    feat: route web query to Soft Basic Circle lab

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 import 'app/my_lock_app.dart';
 import 'app/my_lock_lock_app.dart';
+import 'dev/soft_basic_circle_lab.dart';
 import 'lock_engine/shape_spec/shape_spec_registry.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ShapeSpecRegistry.instance.load();
+
+  if (kIsWeb &&
+      Uri.base.queryParameters['lab'] == 'soft-basic-circle') {
+    runApp(const SoftBasicCircleLabApp());
+    return;
+  }
+
   runApp(const MyLockApp());
 }
 
