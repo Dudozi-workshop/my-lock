927c726e6e00ffb75b3fba08aa46cc20265af6dd
Dudozi-workshop <rlatkd5959@gmail.com>
2026-09-25 21:35:56 +0900

    feat: route sea turtle lab on web query

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 import 'app/my_lock_app.dart';
 import 'app/my_lock_lock_app.dart';
+import 'features/labs/sea_turtle_runtime_lab_screen.dart';
 import 'lock_engine/sea_turtle_runtime_poc.dart';
 import 'lock_engine/shape_spec/shape_spec_registry.dart';
 
@@ -9,6 +11,18 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ShapeSpecRegistry.instance.load();
   await SeaTurtleRuntimePoc.instance.load();
+
+  if (kIsWeb &&
+      Uri.base.queryParameters['lab'] == 'sea-turtle-runtime') {
+    runApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SeaTurtleRuntimeLabScreen(),
+      ),
+    );
+    return;
+  }
+
   runApp(const MyLockApp());
 }
 
